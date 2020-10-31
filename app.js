2d4fa94a1ce354c15a80244c437bff770e4a4aef
Kwok <kwokrenx@gmail.com>
2020-10-31 14:34:11 -0400

    displaying question choices

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ request.send(null)
 let triviaQuestions = JSON.parse(request.responseText);
 
 let randomIndex = Math.floor(Math.random() * triviaQuestions.length)
-let counter = 0
 
 // console.log(triviaQuestions[randomIndex]);
 
-// for every question in the Array, display it on the page one by one randomly 
 // every question will have the correct answer and the incorrect answers
 // radio buttons for each of the questions
 // if the value of the input field (radio button) is correct, then counter += 1
 // In the end, display message about score and percentage
-// Try again button to refresh page
 
 let alreadyAnswered = {}
 
@@ -26,16 +23,30 @@ function shuffle(arr) {
 }
 
 let shuffledQuestions = shuffle(triviaQuestions)
+console.log(shuffledQuestions);
 
 let questionSelector = () =>  {
-    for (let i = 0; i <= shuffledQuestions.length; i++){
-        console.log(i)
-        if (i !== 21) {
-            let $questionDiv = $("<div>")
+    let counter = 0
+    for (let i = 0; i < 10; i++){
+        if (i !== 9) {
+            let index = i
+            let $questionDiv = $("<div>").addClass('question-div')
             $questionDiv.addClass(`${i}`).text(`${shuffledQuestions[i].question}`)
             if (alreadyAnswered[`${shuffledQuestions[i].question}`]) {
                 continue
             } else {
+                let choices = shuffledQuestions[i].incorrect
+                choices.push(shuffledQuestions[i].correct)
+                let shuffledChoices = shuffle(choices);
+                console.log(shuffledChoices.length);
+                for (let i = 0; i < shuffledChoices.length; i++){
+                    console.log(i)
+                    $(".question-choices-container")
+                    $(".question-choices-container")
+                    .append($(".question-choices")
+                    .append(`<input type="radio" name="${shuffledQuestions[index].question}" id="${shuffledChoices[i]}" value="${shuffledChoices[i]}">`)
+                    .append(`<label for="${shuffledChoices[i]}">${shuffledChoices[i]}<label>`))
+                }
                 $(".question-container").append($questionDiv)
                 alreadyAnswered[`${shuffledQuestions[i].question}`] = 1
                 break;
@@ -55,12 +66,19 @@ let questionSelector = () =>  {
 
 $(".begin").on('click', questionSelector);
 $(".begin").on('click', () => {
-    $(".next").addClass('show')
+    // $(".next").addClass('show')
+    $(".submit").addClass('show');
     $(".begin").addClass('hidden')
 });
+$(".next").on('click', () => {
+    $(".question-choices").find('input').remove();
+})
+$(".next").on('click', () => {
+    $(".question-choices").find('label').remove();
+})
 $(".next").on('click', questionSelector);
 $(".next").on('click', () => {
-    $(".question-container").find('div').first().remove();
+    $(".question-container").find('.question-div').first().remove();
 })
 $(".try-again").on('click', () => {
     location.reload();
